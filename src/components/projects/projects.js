7c2511a922323a00e7c8fb15c977ad02941ee481
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-07-10 15:43:31 +1000

    added projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,42 +1,38 @@
 import React from 'react';
-import './projects.css';
-import '../style.css';
+import 'components/style/Projects.css';
+import 'components/style/Style.css';
 
 
 function Projects () {
     return (
         <section id="projects" class="projects-section">
-            <h1 class="h1-class">Projects</h1>
+            <h1 className="h1-class">Projects</h1>
             <p>Somethings I've built:</p>
             <br></br>
-            <div class="images">
+            <div className="images">
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img  class="small-image" src="./Assets/Plexels-1.jpg" alt="city and river"/></a>
-                    <figcaption>First application</figcaption>
+                    <a href="https://nakisa-alipour.github.io/Password-Generator/" target="_blank"><img  className="small-image" src="assets/projects-images/Password Generator.jpg" alt="white back ground page with password generator"/></a>
+                    <figcaption>Password Generator</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-nicolas-poupart-2360569.jpg" alt="city and sea"/></a>
-                    <figcaption>Second application</figcaption>
+                    <a href="https://nakisa-alipour.github.io/Timed-Coding-Quiz/" target="_blank"><img className="small-image" src="assets/projects-images/Multiple Choice Quiz.jpg" alt="page with multiple choice quiz"/></a>
+                    <figcaption>Multiple Choice Quiz</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-stephan-müller-1473215.jpg" alt="happy face"/></a>
-                    <figcaption>Third application</figcaption>
+                    <a href="https://nakisa-alipour.github.io/Work-Day-Scheduler/" target="_blank"><img className="small-image" src="assets/projects-images/Work Day Scheduler.jpg" alt="page with gray,red and green rows"/></a>
+                    <figcaption>Work Day Scheduler</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-pixabay-220067.jpg" alt="dark background with small light spots"/></a>
-                    <figcaption>Forth application</figcaption>
+                    <a href="https://nakisa-alipour.github.io/5-day-weather-forecast/" target="_blank"><img className="small-image" src="assets/projects-images/Weather Dashboard.jpg" alt="page with blue sky and clouds background"/></a>
+                    <figcaption>Five Day Weather Dashboard</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-pixabay-259915.jpg" alt="brick wall"/></a>
-                    <figcaption>Fifth application</figcaption>
+                    <a href="https://note-taker-heroku-app.herokuapp.com/" target="_blank"><img className="small-image" src="assets/projects-images/Note taker.jpg" alt="white page background with blue Note Taker header"/></a>
+                    <figcaption>Note Taker</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-toni-cuenca-585752.jpg" alt="coffee and a keybord"/></a>
-                    <figcaption>Sixth application</figcaption>
-                </figure>
-                <figure>
-                    <a href="https://www.google.com" target="_blank"><img class="small-image" src="./Assets/pexels-maria-tyutina-954599.jpg" alt="hello"/></a>
-                    <figcaption>Seventh application</figcaption>
+                    <a href="https://pwa-note-modifier-c775fafcd0b7.herokuapp.com/" target="_blank"><img className="small-image" src="assets/projects-images/Text Editor.jpg" alt="black background page with Just Another Text Editor title"/></a>
+                    <figcaption>Text Editor</figcaption>
                 </figure>
             </div>
         </section>
