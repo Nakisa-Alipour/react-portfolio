c95b40638ba5c282c9b0f88ee0109ca902492c57
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-07-10 14:43:54 +1000

    footer and the required images

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import './Footer.css';
-import '../style.css';
+import 'components/style/Footer.css';
+import 'components/style/style.css'; ;
 
 const Footer = () => {
   return (
     <footer>
-      <div class="image-container">
-            <img src="./Assets/linkedin-2.png" alt="linkedin logo color:#16AEC8FA and Background:none"/>
-            <div class="info-box">Linkdin- Coming Soon</div>
+      <div className="image-container">
+            <img src="assets/linkedin-2.png" alt="linkedin logo color:#16AEC8FA and Background:none"/>
+            <div className="info-box">Linkdin- Coming Soon</div>
         </div>
-        <div class="image-container">
-            <img src="./Assets/mail.png" alt="mail sign"/>
-            <div class="info-box">email address- Coming Soon</div>
+        <div className="image-container">
+            <img src="assets/mail.png" alt="mail sign"/>
+            <div className="info-box">email address- Coming Soon</div>
         </div>
-        <div class="image-container">
-            <img src="./Assets/phone.png" alt="phone sign"/>
-            <div class="info-box">Phone number - Coming Soon</div>
+        <div className="image-container">
+            <img src="assets/phone.png" alt="phone sign"/>
+            <div className="info-box">Phone number - Coming Soon</div>
         </div>
-        <div class="image-container">
-            <img src="./Assets/github.png" alt="Github sign"/>
-            <div class="info-box">Github URL- Coming Soon</div>
+        <div className="image-container">
+            <img src="assets/github.png" alt="Github sign"/>
+            <div className="info-box">Github URL- Coming Soon</div>
         </div>
     </footer>
   );
