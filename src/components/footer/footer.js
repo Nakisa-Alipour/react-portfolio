a5f9ce6d13790cde079ce40215e18d3bbf3e63ad
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-07-11 16:18:01 +1000

    package.json and footer images

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,19 +6,19 @@ const Footer = () => {
   return (
     <footer>
       <div className="image-container">
-            <img src="assets/linkedin-2.png" alt="linkedin logo color:#16AEC8FA and Background:none"/>
+            <img src="./assets/Avatar.png" alt="linkedin logo color:#16AEC8FA and Background:none"/>
             <div className="info-box">Linkdin- Coming Soon</div>
         </div>
         <div className="image-container">
-            <img src="assets/mail.png" alt="mail sign"/>
+            <img src="./assets/mail.png" alt="mail sign"/>
             <div className="info-box">email address- Coming Soon</div>
         </div>
         <div className="image-container">
-            <img src="assets/phone.png" alt="phone sign"/>
+            <img src="./assets/phone.png" alt="phone sign"/>
             <div className="info-box">Phone number - Coming Soon</div>
         </div>
         <div className="image-container">
-            <img src="assets/github.png" alt="Github sign"/>
+            <img src="./assets/github.png" alt="Github sign"/>
             <div className="info-box">Github URL- Coming Soon</div>
         </div>
     </footer>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,27 +11,27 @@ function Projects () {
             <br></br>
             <div className="images">
                 <figure>
-                    <a href="https://nakisa-alipour.github.io/Password-Generator/" target="_blank"><img  className="small-image" src="assets/projects-images/Password Generator.jpg" alt="white back ground page with password generator"/></a>
+                    <a href="https://nakisa-alipour.github.io/Password-Generator/"><img  className="small-image" src="assets/projects-images/Password Generator.jpg" alt="white back ground page with password generator"/></a>
                     <figcaption>Password Generator</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://nakisa-alipour.github.io/Timed-Coding-Quiz/" target="_blank"><img className="small-image" src="assets/projects-images/Multiple Choice Quiz.jpg" alt="page with multiple choice quiz"/></a>
+                    <a href="https://nakisa-alipour.github.io/Timed-Coding-Quiz/"><img className="small-image" src="assets/projects-images/Multiple Choice Quiz.jpg" alt="page with multiple choice quiz"/></a>
                     <figcaption>Multiple Choice Quiz</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://nakisa-alipour.github.io/Work-Day-Scheduler/" target="_blank"><img className="small-image" src="assets/projects-images/Work Day Scheduler.jpg" alt="page with gray,red and green rows"/></a>
+                    <a href="https://nakisa-alipour.github.io/Work-Day-Scheduler/"><img className="small-image" src="assets/projects-images/Work Day Scheduler.jpg" alt="page with gray,red and green rows"/></a>
                     <figcaption>Work Day Scheduler</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://nakisa-alipour.github.io/5-day-weather-forecast/" target="_blank"><img className="small-image" src="assets/projects-images/Weather Dashboard.jpg" alt="page with blue sky and clouds background"/></a>
+                    <a href="https://nakisa-alipour.github.io/5-day-weather-forecast/"><img className="small-image" src="assets/projects-images/Weather Dashboard.jpg" alt="page with blue sky and clouds background"/></a>
                     <figcaption>Five Day Weather Dashboard</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://note-taker-heroku-app.herokuapp.com/" target="_blank"><img className="small-image" src="assets/projects-images/Note taker.jpg" alt="white page background with blue Note Taker header"/></a>
+                    <a href="https://note-taker-heroku-app.herokuapp.com/"><img className="small-image" src="assets/projects-images/Note taker.jpg" alt="white page background with blue Note Taker header"/></a>
                     <figcaption>Note Taker</figcaption>
                 </figure>
                 <figure>
-                    <a href="https://pwa-note-modifier-c775fafcd0b7.herokuapp.com/" target="_blank"><img className="small-image" src="assets/projects-images/Text Editor.jpg" alt="black background page with Just Another Text Editor title"/></a>
+                    <a href="https://pwa-note-modifier-c775fafcd0b7.herokuapp.com/"><img className="small-image" src="assets/projects-images/Text Editor.jpg" alt="black background page with Just Another Text Editor title"/></a>
                     <figcaption>Text Editor</figcaption>
                 </figure>
             </div>
