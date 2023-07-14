1562d15b85d2550890c5450809593eefb09dfab1
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-07-14 11:24:11 +1000

    modified directory, added index.css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,6 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-import React, { useState } from "react";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import About from "./components/about";
-import Projects from "./components/projects";
-import Contact from "./components/contact";
-import Resume from "./components/resume";
-
-function App() {
-	const [currentTab, setCurrentTab] = useState("about");
-
-	// This function checks to see which tab is selected and then generates the appropriate tab.
-	const renderTab = () => {
-		switch (currentTab) {
-			case "about":
-				return <About />;
-			case "projects":
-				return <Projects />;
-			case "resume":
-				return <Resume />;
-			case "contact":
-				return <Contact />;
-			default:
-				return null;
-		}
-	};
-
-	return (
-		<div>
-			<div>
-				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-			<div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
